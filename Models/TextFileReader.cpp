14e6a15edc19c45fed05f98d6025c0c2ed45f8c1
Dmitry Morozov <dvmorozov@hotmail.com>
2017-11-26 10:46:10 +0100

    Refactoring.
    https://action.mindjet.com/task/14999771

---
+++ Models/TextFileReader.cpp
@@ -0,0 +1,36 @@
+
+#include "TextFileReader.h"
+
+namespace Bookmarks
+{
+    void TextFileReader::SetFileName(std::wstring fileName)
+    {
+        _fileName = fileName;
+    }
+
+    std::vector<std::wstring> TextFileReader::ReadFile()
+    {
+        FILE *f = _wfopen(_fileName.c_str(), _T("r"));
+        std::vector<std::wstring> lines;
+
+        if (f)
+        {
+            char lineptr[MAX_LINE_LENGTH];
+            while (fgets(lineptr, MAX_LINE_LENGTH, f))
+                if (strlen(lineptr))
+                {
+#ifndef LINUX
+                    wchar_t wline[MAX_LINE_LENGTH];
+                    MultiByteToWideChar(CP_OEMCP, 0, lineptr, -1, wline, MAX_LINE_LENGTH);
+                    lines.push_back(std::wstring(wline));
+#endif
+                }
+
+            fclose(f);
+        }
+        //  https://action.mindjet.com/task/14719680
+        //  Временный файл удаляется.
+        _wremove(_fileName.c_str());
+        return lines;
+    }
+}
